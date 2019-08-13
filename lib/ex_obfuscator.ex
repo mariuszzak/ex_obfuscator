97f1e4e17ea64e395e74e6c8d28226e20917121a
Mariusz <mariuszzak21@gmail.com>
2019-08-13 20:00:10 +0200

    Initial commit

---
+++ lib/ex_obfuscator.ex
@@ -0,0 +1,18 @@
+defmodule ExObfuscator do
+  @moduledoc """
+  Documentation for ExObfuscator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExObfuscator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_obfuscator_test.exs
@@ -0,0 +1,8 @@
+defmodule ExObfuscatorTest do
+  use ExUnit.Case
+  doctest ExObfuscator
+
+  test "greets the world" do
+    assert ExObfuscator.hello() == :world
+  end
+end
