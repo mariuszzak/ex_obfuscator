4993e0cc3268bbf745113937eab5bd6255f0490f
Mariusz <mariuszzak21@gmail.com>
2019-08-13 20:16:34 +0200

    Cover the case when a blacklisted key is String type

--- test/ex_obfuscator_test.exs
+++ test/ex_obfuscator_test.exs
@@ -2,7 +2,7 @@ defmodule ExObfuscatorTest do
   use ExUnit.Case
   doctest ExObfuscator
 
-  test "obfuscates a map with blacklisted keys" do
+  test "obfuscates a map with blacklisted atom keys" do
     input = %{
       blacklisted_key: "Some value",
       regular_key: "Other value"
@@ -14,5 +14,19 @@ defmodule ExObfuscatorTest do
     }
 
     assert ExObfuscator.call(input, [:blacklisted_key]) == expected_output
+  end
+
+  test "obfuscates a map with blacklisted string keys" do
+    input = %{
+      "blacklisted_key" => "Some value",
+      "regular_key" => "Other value"
+    }
+
+    expected_output = %{
+      "blacklisted_key" => "Som*******",
+      "regular_key" => "Other value"
+    }
+
+    assert ExObfuscator.call(input, ["blacklisted_key"]) == expected_output
   end
 end
